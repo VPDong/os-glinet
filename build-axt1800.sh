4ae57c8cb264b527673667d1a2412d58ac94f607
VPDong <vpdong@qq.com>
2022-07-04 00:10:01 +0800

    init

--- build-axt1800.sh
+++ build-axt1800.sh
@@ -16,17 +16,27 @@ fi
 
 echo "start to build..."
 
-#clone source 
+#clone source
 git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
-rm -rf $base/gl-infra-builder/feeds/glinet/helloworld || pwd
-cp -r custom/  $base/gl-infra-builder/feeds/custom/
-cp -r custom.yml $base/gl-infra-builder/profiles
-cd $base/gl-infra-builder
+if [ -d $base/gl-infra-builder/feeds/glinet/helloworld ]; then
+   rm -rf $base/gl-infra-builder/feeds/glinet/helloworld
+fi
+hasCustom="false"
+if [ -f custom.yml ]; then 
+    hasCustom="true"
+	cp -r custom.yml $base/gl-infra-builder/profiles
+	cp -r custom/  $base/gl-infra-builder/feeds/custom/
+fi
 
 #setup source
+cd $base/gl-infra-builder
 python3 setup.py -c config-wlan-ap.yml
 cd wlan-ap/openwrt
-./scripts/gen_config.py target_wlan_ap-gl-axt1800 luci custom
+if[ "$hasCustom" == "false" ]; then
+    /scripts/gen_config.py target_wlan_ap-gl-axt1800 luci
+else
+    /scripts/gen_config.py target_wlan_ap-gl-axt1800 luci custom
+fi
 ./scripts/feeds update -a 
 ./scripts/feeds install -a
 make defconfig
--- readme.md
+++ readme.md
@@ -1,2 +1,37 @@
-# os-glinet
-./build.sh ~
+# 仓库说明
+
+> 1、本仓库是在纯官方编译指导的基础上增加了custom，所有的操作都是明文可见，绝不含任何后门操作。
+>
+> 2、也可以fork之后将custom.yml和custom文件夹删除后，即是官方指导的纯镜像。
+>
+> 3、具体请见build-axt1800.sh以及build-axt1800.yml。
+
+
+
+### 一、官方固件
+
+在开始之前，先放上[官方固件](https://dl.gl-inet.cn/?model=axt1800)的下载地址，以防止任何的不测。其他问题见[官方论坛](https://forum.gl-inet.cn/)。
+
+
+
+### 二、自编固件
+
+目录介绍如下：
+
+```
+.
+├── .github // 隐藏文件
+│   └── workflows // 用于github的流水线配置
+│       ├── build-axt1800.yml // 具体的流水线作业定义
+├── build-axt1800.sh // 具体fireware的编译脚本
+├── custom.yml // 自定义插件配置
+├── custom     // 自定义插件依赖
+│   └── helloworld
+│       ├── dns2socks...
+│       ├── ipt2socks...
+│       ├── microsocks...
+│       └── redsocks2...
+├── .gitignore // 请勿删除
+└── readme.md
+```
+
